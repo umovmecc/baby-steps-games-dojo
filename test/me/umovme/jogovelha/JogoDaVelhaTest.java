07fb40259ada41d49b983f4d0e6fef07136dc4d6
Guilherme Elias <guilherme.elias@gmail.com>
2013-07-04 11:32:27 -0300

    Validando jogada invalida

--- test/me/umovme/jogovelha/JogoDaVelhaTest.java
+++ test/me/umovme/jogovelha/JogoDaVelhaTest.java
@@ -1,5 +1,6 @@
 package me.umovme.jogovelha;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -10,6 +11,11 @@ public class JogoDaVelhaTest {
 	public void deveRetornarTrueQuandoJogadaValida() {
 		Tabuleiro tabuleiro = new Tabuleiro();
 		assertTrue(tabuleiro.jogar(1, 1, "O"));
+	}
 
+	@Test
+	public void deveRetornarFalsoQuandoJogadaInvalidaForaDoTabuleiro() {
+		Tabuleiro tabuleiro = new Tabuleiro();
+		assertFalse(tabuleiro.jogar(4, 5, "X"));
 	}
 }
--- test/me/umovme/jogovelha/Tabuleiro.java
+++ test/me/umovme/jogovelha/Tabuleiro.java
@@ -5,8 +5,12 @@ public class Tabuleiro {
 	private final String[][] casas = new String[3][3];
 
 	public Boolean jogar(int i, int j, String jogador) {
-		casas[i][j] = jogador;
-		return true;
+		try {
+			casas[i][j] = jogador;
+			return true;
+		} catch (IndexOutOfBoundsException ex) {
+			return false;
+		}
 	}
 
 }
