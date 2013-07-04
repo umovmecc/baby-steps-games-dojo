b7cbffa3716070d8c5a2c64c62201652f478734a
Guilherme Elias <guilherme.elias@gmail.com>
2013-07-04 11:23:41 -0300

    Criado primeira jogada

--- test/me/umovme/jogovelha/JogoDaVelhaTest.java
+++ test/me/umovme/jogovelha/JogoDaVelhaTest.java
@@ -1,6 +1,15 @@
 package me.umovme.jogovelha;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 
 public class JogoDaVelhaTest {
+	@Test
+	public void deveRetornarTrueQuandoJogadaValida() {
+		Tabuleiro tabuleiro = new Tabuleiro();
+		assertTrue(tabuleiro.jogar(1, 1));
 
+	}
 }
---
+++ test/me/umovme/jogovelha/Tabuleiro.java
@@ -0,0 +1,10 @@
+package me.umovme.jogovelha;
+
+public class Tabuleiro {
+
+	public Boolean jogar(int i, int j) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
