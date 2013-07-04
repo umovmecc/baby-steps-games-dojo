cdd4bc3e9ef337a2d1cbc4df4ef1bed46bfb6f74
Guilherme Elias <guilherme.elias@gmail.com>
2013-07-04 13:02:39 -0300

    refatorado nome da classe que envolve a logica do jogo

--- src/me/umovme/jogovelha/JogoDaVelha.java
+++ src/me/umovme/jogovelha/JogoDaVelha.java
@@ -1,11 +1,14 @@
 package me.umovme.jogovelha;
 
-public class Tabuleiro {
+public class JogoDaVelha {
 
 	private final String[][] casas = new String[3][3];
 
 	public Boolean jogar(int i, int j, String jogador) {
 		try {
+			if (casas[i][j] != null)
+				return false;
+
 			casas[i][j] = jogador;
 			return true;
 		} catch (IndexOutOfBoundsException ex) {
--- test/me/umovme/jogovelha/JogoDaVelhaTest.java
+++ test/me/umovme/jogovelha/JogoDaVelhaTest.java
@@ -3,19 +3,33 @@ package me.umovme.jogovelha;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class JogoDaVelhaTest {
+
+	private JogoDaVelha jogoDaVelha;
+
+	@Before
+	public void setUp() {
+		jogoDaVelha = new JogoDaVelha();
+	}
+
 	@Test
 	public void deveRetornarTrueQuandoJogadaValida() {
-		Tabuleiro tabuleiro = new Tabuleiro();
-		assertTrue(tabuleiro.jogar(1, 1, "O"));
+		assertTrue(jogoDaVelha.jogar(1, 1, "O"));
 	}
 
 	@Test
 	public void deveRetornarFalsoQuandoJogadaInvalidaForaDoTabuleiro() {
-		Tabuleiro tabuleiro = new Tabuleiro();
-		assertFalse(tabuleiro.jogar(4, 5, "X"));
+		assertFalse(jogoDaVelha.jogar(4, 5, "X"));
 	}
+
+	@Test
+	public void deveRetornarFalsoQuandoJogarEmUmaJogadaExistente() {
+		jogoDaVelha.jogar(1, 1, "O");
+		assertFalse(jogoDaVelha.jogar(1, 1, "X"));
+	}
+
 }
